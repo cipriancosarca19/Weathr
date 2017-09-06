2c70a9a6c3144842956acda6f393eec0b4c906f4
Manpreet "Mannie" Singh <mannie.faux@gmail.com>
2017-09-06 16:55:23 -0400

    Add Normalize.css to project

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import registerServiceWorker from 'utils/registerServiceWorker';
 
 import Weathr from 'components/Weathr';
+import 'index.css';
 
 ReactDOM.render(<Weathr />, document.getElementById('root'));
 
