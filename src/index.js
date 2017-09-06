ef2c48f88b12da556647ebea668f53186a627439
Manpreet "Mannie" Singh <mannie.faux@gmail.com>
2017-09-06 13:53:08 -0400

    Add `./src` to module resolution paths
    * Add cross-env to project
    * Use cross-env in `package.json` to set `NODE_PATH` and allow absolute referencing of files in `./src`

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import registerServiceWorker from './utils/registerServiceWorker';
+import registerServiceWorker from 'utils/registerServiceWorker';
 
-import Weathr from './components/Weathr';
+import Weathr from 'components/Weathr';
 
 ReactDOM.render(<Weathr />, document.getElementById('root'));
 
