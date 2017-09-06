db2e133a31574fac3cdd55ffcda2af1ec2455805
Manpreet "Mannie" Singh <mannie.faux@gmail.com>
2017-09-06 14:05:18 -0400

    Fix reference error in deep prop utility's `set` function

--- src/utils/deep/set.js
+++ src/utils/deep/set.js
@@ -5,7 +5,7 @@ function set(obj, path, value) {
   if (!obj || path.length === 0) return undefined;
 
   obj = _.cloneDeep(obj);
-  const root = obj;
+  const objRoot = obj;
 
   for (let i = 0; i < path.length; i++) {
     if (!obj || !obj.hasOwnProperty(path[i])) obj[path[i]] = {};
@@ -13,7 +13,7 @@ function set(obj, path, value) {
     obj = obj[path[i]];
   }
 
-  return rootObj;
+  return objRoot;
 }
 
 export default set;
