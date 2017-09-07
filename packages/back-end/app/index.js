a66d9534daeaf9d5faf2474e921a0ae53c53cd1b
Manpreet "Mannie" Singh <mannie.faux@gmail.com>
2017-09-07 15:13:13 -0400

    Create initial route in Hapi.js
    * Write some helper functions to generate API request URLs
    * Create empty route at `/weathr/${query}`

--- packages/back-end/app/index.js
+++ packages/back-end/app/index.js
@@ -4,10 +4,24 @@ const Hapi = require('hapi');
 
 const weathr = new Hapi.Server();
 
+const getGeocodeUrl = query =>
+  `https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDjh11j9IJHALxDJd4z--VMuTLpAEbANyA&address=${query}`;
+const getForecastURL = (lat, long) =>
+  `https://api.darksky.net/forecast/9c05b6662a30448a33162cc584b4a264/${lat},${long}`;
+
 weathr.connection({
   port: 3001,
   host: '0.0.0.0',
 });
+
+weathr.route({
+  method: 'GET',
+  path: '/weathr/{query}',
+  handler: (request, reply) => {
+    reply(`TODO: Geocode ${request.params.query}, then get and reply with the forecast`);
+  },
+});
+
 weathr.start(err => {
   if (err) throw err;
   console.log(`Server running at ${weathr.info.uri}`);
