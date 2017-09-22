e670f0f9c2f748ca300a982ccbd76903d1c3d2b0
Manpreet "Mannie" Singh <mannie.faux@gmail.com>
2017-09-22 01:49:32 -0400

    Remove unneccessary font weights from theme

--- packages/front-end/src/theme/index.js
+++ packages/front-end/src/theme/index.js
@@ -1,7 +1,6 @@
 import colors from './colors';
 
 export default {
-  weights: [ 400, 700 ],
   colors,
   font: '"Open Sans", -apple-system, BlinkMacSystemFont, sans-serif',
   monospace: 'Inconsolata, "SF Mono", "Roboto Mono", Menlo, monospace',
